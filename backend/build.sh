15a2b242221d69dd51289e29c7504d05246c4221
2010ibnsina <ibnsina2010@cseku.ac.bd>
2026-06-27 20:03:53 +0600

    admin

--- backend/build.sh
+++ backend/build.sh
@@ -5,3 +5,14 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate
+
+python manage.py shell << EOF
+from django.contrib.auth.models import User
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser(
+        "admin",
+        "admin@example.com",
+        "Admin@12345"
+    )
+EOF
