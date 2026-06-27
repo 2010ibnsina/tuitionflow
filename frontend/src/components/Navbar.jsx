e7da30ba1de4df6093571c7b99f8108f96067ad4
2010ibnsina <ibnsina2010@cseku.ac.bd>
2026-06-27 20:54:42 +0600

    update code

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -81,13 +81,16 @@ function Navbar() {
           {/* Mobile Toggle */}
 
           <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+  className="navbar-toggler"
+  type="button"
+  data-bs-toggle="collapse"
+  data-bs-target="#navbarNav"
+  aria-controls="navbarNav"
+  aria-expanded="false"
+  aria-label="Toggle navigation"
+>
+  <span className="navbar-toggler-icon"></span>
+</button>
 
           <div
             className="collapse navbar-collapse justify-content-end"
